712f026e439c3aa6d1bbcdeaeb754dc5760b3d10
duccline <hugostskyline@gmail.com>
2025-06-01 21:05:30 -0400

    blablabla

--- assets/components/SearchBar.tsx
+++ assets/components/SearchBar.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { Image, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
 
 export default function SearchBar(){
+    const[texto, setTexto] = useState('');
     return(
-        <View>
-            <Image/>
-            <TextInput></TextInput>
+        <View style={styles.containerHH}>
+            <Image source={{uri: 'https://img.icons8.com/ios7/600/FFFFFF/search.png'}}/>
+            <TextInput placeholder='Procurar...' onChangeText={setTexto} value={texto} style={styles.inputHH}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerHH:{
+
+    },
+    
+    inputHH:{
+
+    },
+
+    imageHH:{
+
+    }
+})
+
